938c9d2a90efe94afedfdcad7b26347f780d1692
Christopher Hsiao <hsiao.christopher@gmail.com>
2016-08-30 11:54:23 -0700

    private w/o token

---
+++ config/private.py
@@ -0,0 +1,15 @@
+AREAS = ['scz']
+SLACK_CHANNEL = 'housing-sc'
+SLACK_TOKEN = "TOKEN HERE"
+NEIGHBORHOODS = ["aptos","boulder creek","capitola","santa cruz","scotts valley","soquel","watsonville"]
+BOXES = {
+    "santacruz": [
+        [36.981908, -122.050836],
+        [36.962531, -122.003853],
+    ]
+}
+
+TRANSIT_STATIONS = {
+    "sc_metro": [36.990236, -122.0365491]
+}
+MAX_TRANSIT_DIST = 2
